fdbecceb4e40f9e8bc4d7d0b1479ce79e58b7498
mustaphachqoubi <mustaphachqoubi88@gmail.com>
2022-12-24 18:16:48 +0100

    trying to add some js

---
+++ sz/app/static/app/js/myscript.js
@@ -0,0 +1,7 @@
+let menu = document.getElementById("menu");
+let menuList = document.getElementById("menuList");
+
+menu.addEventListener("click", () => {
+    menuList.classList === menuList.classList. ? alert('yes') : alert('no') 
+    // menuList.classList === "hidden" ? menuList.classList.remove("hidden") : menuList.classList.add("hidden") 
+});
